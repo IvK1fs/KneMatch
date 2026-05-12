1c633eca33cd152acbfe5452c010b2daceb377f3
João David Salomão <joaodavidsalomaooliveira@gmail.com>
2026-05-12 13:26:15 -0300

    atualizados

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,1 +1,11 @@
+import express from 'express';
 
+const app = express();
+
+app.listen(3000, () => {
+    console.log("Servidor na porta 3000");
+});
+
+app.get('/', (req, res) =>
+    res.send('<h1 style="color: green">CRIANDO UM SERVIDOR COM EXPRESS.JS</h1>')
+);
